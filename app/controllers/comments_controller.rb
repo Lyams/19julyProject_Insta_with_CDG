2dc7b34cbfb8f7d652be4d143c4c32fbb914d0e6
Ilya L <lyamsh@yandex.ru>
2021-07-21 02:54:23 +0300

    Add association post, user, comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,19 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
   end
 
   def create
-    Comment.create(comment_params)
+    comment = Comment.new(comment_params)
+    #comment.user_id = current_user.id
+    comment.save
     redirect_to post_path(comment_params[:post_id])
   end
 
   private
 
   def comment_params
-  params.require(:comment).permit(:commentary, :post_id)
- end
+    params.require(:comment).permit(:commentary, :post_id, :user_id)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
+  before_action :validate_autorization, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
   # GET /posts or /posts.json
   def index
     @posts = Post.all
@@ -24,7 +25,7 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-
+    @post.user_id = current_user.id
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
@@ -64,8 +65,16 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
     end
 
+    def validate_autorization
+      if current_user == @post.user
+        #TODO: message and work for changed of post
+        true
+      else
+        redirect_back(fallback_location: root_path, notice: "Post cannot be changed by non-author ")
+      end
+    end
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:description, :image)
+      params.require(:post).permit(:description, :image, :user_id)
     end
 end
