303ffbf93734a7cd39d225f66e44e807becab318
Ilya L <lyamsh@yandex.ru>
2021-07-23 00:51:37 +0300

    Change resource post into user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users, only: [] do
+    resources :posts
+  end
   root to: "posts#index"
-  resources :posts
+
   resources :comments, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
