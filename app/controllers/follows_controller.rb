a1aa4e37f76f508fe83427fe65745c3c8f2c1dd5
Ilya L <lyamsh@yandex.ru>
2021-07-25 00:20:51 +0300

    Small change

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,7 +5,7 @@ class FollowsController < ActionController::Base
     follower_user = current_user
     following_user = User.find(params[:following_id])
     Follow.create(follower: follower_user, following: following_user)
-    redirect_to user_followers_path(current_user)
+    redirect_to user_followings_path(current_user)
   end
 
   def destroy
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  include Pundit
+
   before_action :authenticate_user!, except: [:show, :index]
   before_action :set_user
   before_action :set_post, only: %i[ show edit update destroy ]
