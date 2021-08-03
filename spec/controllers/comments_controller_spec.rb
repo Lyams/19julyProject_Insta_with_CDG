9b7988dc5d097718a7c95390e544cae121b0700c
Ilya L <lyamsh@yandex.ru>
2021-08-04 01:01:26 +0300

    Add spec: comments controller

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe CommentsController, type: :controller do
+  let!(:user) { create(:user) }
+  let!(:user2) { create(:second_user)  }
+  let!(:post) { create(:post) }
+
+  context "sign in user" do
+    before { sign_in user2 }
+
+    describe '#create' do
+      let(:params) {{ comment: { commentary: "It is a greek", post_id: post.id, user_id: user2.id } }}
+      subject { process :create, params: params }
+
+      it 'create comment' do
+        expect {subject}.to change {Comment.count}.by(1)
+        is_expected.to redirect_to user_post_path( post.user, post )
+      end
+    end
+  end
+end
