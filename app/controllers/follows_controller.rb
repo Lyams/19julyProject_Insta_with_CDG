195177228c2dec92438dcdbf5a7f1021d6bce80f
Ilya L <lyamsh@yandex.ru>
2021-08-05 02:41:33 +0300

    small changed in FollowsController: redirect_back

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,7 +5,7 @@ class FollowsController < ApplicationController
     follower_user = current_user
     following_user = User.find(params[:following_id])
     Follow.create(follower: follower_user, following: following_user)
-    redirect_to user_followings_path(current_user)
+    redirect_back(fallback_location:user_followings_path(current_user))
   end
 
   def destroy
@@ -13,6 +13,6 @@ class FollowsController < ApplicationController
     following_user = User.find(params[:following_id])
     follow = Follow.find_by(follower: follower_user, following: following_user)
     follow.destroy
-    redirect_to user_followers_path(current_user)
+    redirect_back(fallback_location: user_followers_path(current_user))
   end
 end
