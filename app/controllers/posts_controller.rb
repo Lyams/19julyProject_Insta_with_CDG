fac95958bcf1626c186a41ee3e9309f8c22e718f
Ilya L <lyamsh@yandex.ru>
2021-08-08 04:01:54 +0300

    add test for PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
     @post.user = current_user
     respond_to do |format|
       if @post.save
-        format.html { redirect_to user_post_path(@user, @post), notice: "Post was successfully created." }
+        format.html { redirect_to user_post_path(current_user, @post), notice: "Post was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
