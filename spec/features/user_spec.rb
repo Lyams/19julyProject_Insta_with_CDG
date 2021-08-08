e4b45291a9884478fdc714cfe674cda7611f7f3e
Ilya L <lyamsh@yandex.ru>
2021-08-08 19:40:40 +0300

    style code

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'User', type: :feature do
     visit root_path
     click_link_or_button 'Sign up'
     expect(page).to have_content 'Password (6 characters minimum)'
-      fill_in 'Email', with: 'user@example.com'
-      fill_in 'Bio', with: 'I am testing bot'
-      fill_in 'Name', with: 'Boto Botich'
-      fill_in 'Password', with: 'password1'
-      fill_in 'Password confirmation', with: 'password1'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Bio', with: 'I am testing bot'
+    fill_in 'Name', with: 'Boto Botich'
+    fill_in 'Password', with: 'password1'
+    fill_in 'Password confirmation', with: 'password1'
     click_button 'Sign up'
     expect(page).to have_content 'Posts of'
     expect(page).to have_content 'Welcome! You have signed up successfully.'
@@ -17,6 +17,4 @@ RSpec.describe 'User', type: :feature do
     click_link_or_button 'Log out'
     expect(page).to have_content 'Some of our users'
   end
-
-
 end
